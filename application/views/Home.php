77cfb9812f42d15a99eebe5ae4ccbc81282ceb46
cucrahmat <11.6596@stis.ac.id>
2015-08-08 13:47:30 +0700

    salon
    pertama

---
+++ application/views/Home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>asset/dist/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <head>
+        <title>Masuk</title>
+    </head>
+    <body>
+        <div class="navbar-fixed">
+            <nav class="deep-orange">
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="#">Sass</a></li>
+                        <li><a href="#">Components</a></li>
+                        <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        <p>Masuk</p>
+
+    </body>
+</html>
